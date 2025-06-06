c45f852769f46fd6bf20b0ec798658d0f7f9b547
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2025-06-06 10:13:20 -0300

    Lógica funcionando

--- assets/js/notificacao.js
+++ assets/js/notificacao.js
@@ -24,3 +24,20 @@ const radios = document.querySelectorAll('input[type="radio"][name="tipo_evento"
             outrosInput.value = "";
         });
     });
+
+
+//Fetch no Google Planilhas
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwpeGBvf9tGAmnbbcLpObFaR7cW566D-GRzcI20JOQUHobWmHHGZGoFN9rnplXwIE51/exec';
+    const form = document.getElementById('notificacao_form');
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      fetch(scriptURL, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: new FormData(form)
+      })
+      .then(() => alert("Formulário enviado com sucesso!"))
+      .catch(error => alert("Erro ao enviar o formulário!"));
+    });
